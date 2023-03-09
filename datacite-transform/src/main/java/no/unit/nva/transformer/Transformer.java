3a7cec63f127aadbc795d2fb138d789bb80dbaec
anetteOlli <anette.olli.siiri@sikt.no>
2023-03-09 17:28:51 +0100

    NP-37503-datacite-xml-must-contain-existing-doi, implemented

--- datacite-transform/src/main/java/no/unit/nva/transformer/Transformer.java
+++ datacite-transform/src/main/java/no/unit/nva/transformer/Transformer.java
@@ -1,29 +1,30 @@
 package no.unit.nva.transformer;
 
+import java.io.StringWriter;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import no.unit.nva.transformer.dto.AlternateIdentifierDto;
 import no.unit.nva.transformer.dto.CreatorDto;
 import no.unit.nva.transformer.dto.DataCiteMetadataDto;
 import org.datacite.schema.kernel_4.Resource;
+import org.datacite.schema.kernel_4.Resource.AlternateIdentifiers;
 import org.datacite.schema.kernel_4.Resource.Publisher;
 import org.datacite.schema.kernel_4.Resource.Titles;
 import org.datacite.schema.kernel_4.Resource.Titles.Title;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import java.io.StringWriter;
-import java.util.stream.Collectors;
-
-
 public class Transformer {
+
     public static final String COULD_NOT_GET_JAXB_CONTEXT = "Could not get JAXBContext";
-    private final Resource resource;
-
     private static final JAXBContext jaxbContext = getContext();
-
+    private final Resource resource;
     private final Marshaller marshaller;
 
     /**
      * Transforms a DataCiteMetadataDto to a Datacite Record.
+     *
      * @param dataCiteMetadataDto A DataCiteMetadataDto instance.
      * @throws JAXBException If the XML initialisation fails.
      */
@@ -32,6 +33,18 @@ public class Transformer {
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
         this.resource = new Resource();
         fromDataCiteMetadataDto(dataCiteMetadataDto);
+    }
+
+    /**
+     * Produces an XML string representation of the Datacite record.
+     *
+     * @return String XML.
+     * @throws JAXBException If the record cannot be produced.
+     */
+    public String asXml() throws JAXBException {
+        StringWriter stringWriter = new StringWriter();
+        marshaller.marshal(resource, stringWriter);
+        return stringWriter.toString();
     }
 
     private static JAXBContext getContext() {
@@ -49,6 +62,20 @@ public class Transformer {
         setPublisher(dataCiteMetadataDto);
         setPublicationYear(dataCiteMetadataDto);
         setResourceType(dataCiteMetadataDto);
+        setAlternateIdentifiers(dataCiteMetadataDto);
+    }
+
+    private void setAlternateIdentifiers(DataCiteMetadataDto dataCiteMetadataDto) {
+        var alternateIdentifiers = createAlternativeIdentifiers(dataCiteMetadataDto.getAlternateIdentifiers());
+        resource.setAlternateIdentifiers(alternateIdentifiers);
+    }
+
+    private AlternateIdentifiers createAlternativeIdentifiers(List<AlternateIdentifierDto> alternateIdentifierDtos) {
+        var alternateIdentifiers = new AlternateIdentifiers();
+        alternateIdentifiers.getAlternateIdentifier().addAll(alternateIdentifierDtos.stream().map(
+            AlternateIdentifierDto::asAlternateIdentifier).collect(
+            Collectors.toList()));
+        return alternateIdentifiers;
     }
 
     private void setResourceType(DataCiteMetadataDto dataCiteMetadataDto) {
@@ -80,20 +107,9 @@ public class Transformer {
     private void setResourceCreators(DataCiteMetadataDto dataCiteMetadataDto) {
         var creators = new Resource.Creators();
         dataCiteMetadataDto.getCreator().stream()
-                .map(CreatorDto::toCreator)
-                .collect(Collectors.toList())
-                .forEach(creator -> creators.getCreator().add(creator));
+            .map(CreatorDto::toCreator)
+            .collect(Collectors.toList())
+            .forEach(creator -> creators.getCreator().add(creator));
         resource.setCreators(creators);
-    }
-
-    /**
-     * Produces an XML string representation of the Datacite record.
-     * @return String XML.
-     * @throws JAXBException If the record cannot be produced.
-     */
-    public String asXml() throws JAXBException {
-        StringWriter stringWriter = new StringWriter();
-        marshaller.marshal(resource, stringWriter);
-        return stringWriter.toString();
     }
 }
--- datacite-transform/src/test/java/no/unit/nva/transformer/TransformationTest.java
+++ datacite-transform/src/test/java/no/unit/nva/transformer/TransformationTest.java
@@ -1,8 +1,10 @@
 package no.unit.nva.transformer;
 
+import no.unit.nva.transformer.dto.AlternateIdentifierDto;
 import no.unit.nva.transformer.dto.CreatorDto;
 import no.unit.nva.transformer.dto.DataCiteMetadataDto;
 import no.unit.nva.transformer.dto.IdentifierDto;
+import no.unit.nva.transformer.dto.IdentifierDto.IdentifierType;
 import no.unit.nva.transformer.dto.PublisherDto;
 import no.unit.nva.transformer.dto.ResourceTypeDto;
 import no.unit.nva.transformer.dto.TitleDto;
@@ -11,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import javax.xml.bind.JAXBException;
 import java.util.List;
 
+import static java.util.Objects.nonNull;
 import static no.unit.nva.transformer.dto.CreatorDto.SEPARATOR;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -18,6 +21,7 @@ import static org.hamcrest.Matchers.not;
 
 class TransformationTest {
 
+    public static final String ANY_DOI = "10.5072/example-full";
     public static final String ANY_URI = "https://example.org/123";
     public static final String ANY_NAME = "Wallace, Cornelius";
     public static final String ANY_TITLE = "A long, slow depressing march";
@@ -28,8 +32,8 @@ class TransformationTest {
 
     @Test
     void dynamoRecordDtoReturnsTransformedXmlWhenInputIsValid() throws JAXBException {
-        var record = generateRecord(ANY_URI, ANY_NAME, ANY_TITLE, ANY_YEAR,
-                ANY_PUBLISHER, ANY_RESOURCE_TYPE);
+        var record = generateRecord(ANY_DOI, ANY_NAME, ANY_TITLE, ANY_YEAR,
+                                    ANY_PUBLISHER, ANY_RESOURCE_TYPE, ANY_URI);
         var actual = record.asXml();
 
         assertThat(actual, containsString(ANY_URI));
@@ -38,6 +42,7 @@ class TransformationTest {
         assertThat(actual, containsString(ANY_YEAR));
         assertThat(actual, containsString(ANY_PUBLISHER));
         assertThat(actual, containsString(ANY_RESOURCE_TYPE));
+        assertThat(actual, containsString(ANY_DOI));
     }
 
     @Test
@@ -45,8 +50,8 @@ class TransformationTest {
         var surname = "Higgs";
         var forename = "Boson";
         var name = String.join(SEPARATOR, surname, forename);
-        var record = generateRecord(ANY_URI, name, ANY_TITLE, ANY_YEAR,
-                ANY_PUBLISHER, ANY_RESOURCE_TYPE);
+        var record = generateRecord(ANY_DOI, name, ANY_TITLE, ANY_YEAR,
+                                    ANY_PUBLISHER, ANY_RESOURCE_TYPE, ANY_URI);
         var actual = record.asXml();
         assertThat(actual, containsString(name));
         assertThat(actual, containsString(enclosedString(surname)));
@@ -59,8 +64,8 @@ class TransformationTest {
         String surname = "Higgs";
 
         var name = rank + " " + surname;
-        var record = generateRecord(ANY_URI, name, ANY_TITLE, ANY_YEAR,
-                ANY_PUBLISHER, ANY_RESOURCE_TYPE);
+        var record = generateRecord(ANY_DOI, name, ANY_TITLE, ANY_YEAR,
+                                    ANY_PUBLISHER, ANY_RESOURCE_TYPE, ANY_URI);
         var actual = record.asXml();
         assertThat(actual, containsString(name));
         assertThat(actual, not(containsString(enclosedString(rank))));
@@ -69,58 +74,97 @@ class TransformationTest {
 
     @Test
     void dynamoRecordDtoReturnsTransformedXmlWithResourceTypeGeneralOtherWhenResourceTypeIsNull() throws JAXBException {
-        var record = generateRecord(ANY_URI, ANY_NAME, ANY_TITLE, ANY_YEAR,
-                ANY_PUBLISHER, null);
+        var record = generateRecord(ANY_DOI, ANY_NAME, ANY_TITLE, ANY_YEAR,
+                                    ANY_PUBLISHER, null, ANY_URI);
         var actual = record.asXml();
         assertThat(actual, containsString(RESOURCE_TYPE_OTHER));
+    }
+
+    @Test
+    void dynamoRecordDtoReturnsTransformedXmlWhenDoiIsNull() throws JAXBException {
+        var record = generateRecordWithoutDoi(ANY_NAME, ANY_TITLE, ANY_YEAR,
+                                              ANY_PUBLISHER, null, ANY_URI);
+        var actual = record.asXml();
+        assertThat(actual, containsString("identifierType=\"URL"));
+        assertThat(actual, not(containsString("identifierType=\"DOI")));
     }
 
     private String enclosedString(String string) {
         return ">" + string + "<";
     }
 
-    private DataCiteMetadataDto generateRecord(String identifier,
+    private DataCiteMetadataDto generateRecordWithoutDoi(String creator,
+                                                         String title,
+                                                         String year,
+                                                         String publisher,
+                                                         String resourceType,
+                                                         String resourceIdentifier) {
+        return new DataCiteMetadataDto.Builder()
+                   .withIdentifier(getIdentifierWithUriType(resourceIdentifier))
+                   .withCreator(getCreator(creator))
+                   .withTitle(getTitle(title))
+                   .withPublicationYear(year)
+                   .withPublisher(getPublisher(publisher))
+                   .withResourceType(getResourceType(resourceType))
+                   .build();
+    }
+
+    private IdentifierDto getIdentifierWithUriType(String resourceIdentifier) {
+        return new IdentifierDto.Builder().withType(IdentifierType.URL).withValue(resourceIdentifier).build();
+    }
+
+    private DataCiteMetadataDto generateRecord(String doi,
                                                String creator,
                                                String title,
                                                String year,
                                                String publisher,
-                                               String resourceType) {
+                                               String resourceType,
+                                               String resourceIdentifier) {
         return new DataCiteMetadataDto.Builder()
-                .withIdentifier(getIdentifier(identifier))
-                .withCreator(getCreator(creator))
-                .withTitle(getTitle(title))
-                .withPublicationYear(year)
-                .withPublisher(getPublisher(publisher))
-                .withResourceType(getResourceType(resourceType))
-                .build();
+                   .withIdentifier(getIdentifier(doi))
+                   .withCreator(getCreator(creator))
+                   .withTitle(getTitle(title))
+                   .withPublicationYear(year)
+                   .withPublisher(getPublisher(publisher))
+                   .withAlternateIdentifiers(getAlternateIdentifiers(resourceIdentifier))
+                   .withResourceType(getResourceType(resourceType))
+                   .build();
+    }
+
+    private List<AlternateIdentifierDto> getAlternateIdentifiers(String resourceIdentifier) {
+        return nonNull(resourceIdentifier) ?
+                   List.of(new AlternateIdentifierDto.Builder().withValue(resourceIdentifier).build())
+                   : List.of();
     }
 
     private ResourceTypeDto getResourceType(String resourceType) {
         return new ResourceTypeDto.Builder()
-                .withValue(resourceType)
-                .build();
+                   .withValue(resourceType)
+                   .build();
     }
 
     private PublisherDto getPublisher(String publisher) {
         return new PublisherDto.Builder()
-                .withValue(publisher)
-                .build();
+                   .withValue(publisher)
+                   .build();
     }
 
     private TitleDto getTitle(String title) {
         return new TitleDto.Builder()
-                .withValue(title)
-                .build();
+                   .withValue(title)
+                   .build();
     }
 
     private IdentifierDto getIdentifier(String identifier) {
-        return new IdentifierDto.Builder()
-                .withValue(identifier)
-                .build();
+        return nonNull(identifier) ? new IdentifierDto.Builder()
+                                         .withValue(identifier)
+                                         .withType(IdentifierType.DOI)
+                                         .build()
+                   : null;
     }
 
     private List<CreatorDto> getCreator(String name) {
         return List.of(new CreatorDto.Builder()
-                .withCreatorName(name).build());
+                           .withCreatorName(name).build());
     }
 }
